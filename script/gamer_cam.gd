6d28613403f079250950fc191caa00d2e5453488
P3NU7 <phoebesawchuk@gmail.com>
2025-09-05 02:08:30 -0800

    cam

--- script/gamer_cam.gd
+++ script/gamer_cam.gd
@@ -1,11 +1,18 @@
 extends Camera2D
 
+
+var target_position = Vector2.ZERO
+
 func _ready():
     make_current()
 
 func _process(_delta):
+    aquire_target()
+    global_position = global_position.lerp(target_position, 1.0 - exp(-_delta * 10.0))
+
+func aquire_target():
     var player_nodes = get_tree().get_nodes_in_group("player")
     if player_nodes.size() > 0:
         var player = player_nodes[0] as Node2D
-        global_position = player.global_position
+        target_position = player.global_position
 
