b8c3df5f78b069f99a07c90d19b95123a7cfa1b1
P3NU7 <phoebesawchuk@gmail.com>
2025-09-05 23:02:11 -0800

    broken

--- script/experience_gem.gd
+++ script/experience_gem.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 func _ready():
     $Area2D.area_entered.connect(_on_area_entered)
+    
 
 func _on_area_entered(other_area: Area2D):
     GameEvents.emit_experience_gem_collected(1)
