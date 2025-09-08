1f2d79e9da77ce54b3488db03c6ee632e72eff1a
P3NU7 <phoebesawchuk@gmail.com>
2025-09-07 19:11:13 -0800

    stable

--- script/upgrade_manager.gd
+++ script/upgrade_manager.gd
@@ -18,6 +18,7 @@ func _on_level_up(current_level: int):
     var upgrade_screen_instance = upgrade_card_scene.instantiate()
     add_child(upgrade_screen_instance)
     upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+    upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 
 
@@ -32,3 +33,6 @@ func apply_upgrade(upgrade: AbilityUpgrade):
         }
     else:
         current_upgrades[upgrade.id]["quantity"] += 1
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+    apply_upgrade(upgrade)
