a35ec7e32fd33e18cf1fd5893720a1b811151e43
P3NU7 <phoebesawchuk@gmail.com>
2025-09-09 00:55:45 -0800

    health

--- script/player.gd
+++ script/player.gd
@@ -3,8 +3,14 @@ extends CharacterBody2D
 const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING = 25
 
+@onready var damage_interval_timer = $DamageIntervalTimer
+
+var number_colliding_bodies = 0
+
 func _ready ():
-    pass
+    $CollisionArea2D.body_entered.connect(_on_body_entered)
+    $CollisionArea2D.body_exited.connect(_on_body_exited)
+    damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
 
 func _process(_delta):
     var movement_vector = _get_movement_vector()
@@ -18,6 +24,21 @@ func _get_movement_vector():
     var y_movent = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
     return Vector2(x_movent, y_movent)
 
+func _on_body_entered(other_body: Node2D):
+    number_colliding_bodies += 1
+    check_deal_damage()
 
+    print($HealthComponent.current_health)
 
+func _on_body_exited(other_body: Node2D):
+    number_colliding_bodies -= 1
+
+func check_deal_damage():
+    if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+        return
+    $HealthComponent.damage(1)
+    damage_interval_timer.start()
+
+func on_damage_interval_timer_timeout():
+    check_deal_damage()
 
