e329eda0e09249d5f9336192740a4a4c48e7524a
P3NU7 <phoebesawchuk@gmail.com>
2025-09-09 22:32:09 -0800

    rainbow xp

--- script/experience_gem.gd
+++ script/experience_gem.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
+@export var rainbow_speed: float = 2.0
+var time_passed: float = 0.0
+
 func _ready():
     $Area2D.area_entered.connect(_on_area_entered)
-    
+
+func _process(delta):
+    time_passed += delta
+    var hue = fmod(time_passed * rainbow_speed, 1.0)
+    modulate = Color.from_hsv(hue, 1.0, 1.0)
 
 func _on_area_entered(other_area: Area2D):
     GameEvents.emit_experience_gem_collected(1)
